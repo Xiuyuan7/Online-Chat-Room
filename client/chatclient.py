a17e23d846464312fe7a35ee85fb52ce4247474f
Xiuyuan Wang <xiuyuan7@illinois.edu>
2022-04-15 04:11:56 -0500

    complete registration/login

--- client/chatclient.py
+++ client/chatclient.py
@@ -17,7 +17,7 @@ import threading
 import sys, os, struct
 
 # Any global variables
-BUFFER =  
+BUFFER =  4096
 
 
 
@@ -31,7 +31,7 @@ Returns:
 Hint: you can use the first character of the message to distinguish different types of message
 """
 def accept_messages():
-    
+    pass
 
 
 
@@ -39,21 +39,51 @@ def accept_messages():
 
 if __name__ == '__main__': 
     # TODO: Validate input arguments
-    
+    if len(sys.argv) != 4:
+        print("Wrong number of arguments!")
+        sys.exit()
 
+    if sys.argv[1] != "localhost":
+        print("Wrong server name!")
+        sys.exit()
+
+    if sys.argv[2] != "41025":
+        print("Wrong port number!")
+        sys.exit()
+
+    servername = sys.argv[1]
+    serverip = socket.gethostbyname(servername)
+    port = int(sys.argv[2])
+    username = sys.argv[3]
 
     # TODO: create a socket with UDP or TCP, and connect to the server
     try:
-        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as msg:
         print(f'Failed to create socket. Error Code : {str(msg[0])}\n Message: {msg[1]}')
         sys.exit()
     
-
+    try:
+        sock.connect((serverip, port))
+    except socket.error:
+        print('Failed to connect to server.')
+        sys.exit()
     
     # TODO: Send username to the server and login/register the user
-    
+    sock.send(username.encode())
+    ack = sock.recv(BUFFER).decode()
+    if ack == "1":
+        password = sock.recv(BUFFER).decode()
+        print("Existing user")
+        enteredPassword = input("Enter password: ")
+        while enteredPassword != password:
+            enteredPassword = input("Incorrect password. Please enter again: ")
+    else:
+        print("Creating new user")
+        enteredPassword = input("Enter password: ")
+        sock.send(enteredPassword.encode())
 
+    print("Connection established")
 
     # TODO: initiate a thread for receiving message
     
--- server/chatserver.py
+++ server/chatserver.py
@@ -15,9 +15,9 @@ import threading
 import sys, os, struct
 
 # Any global variables
-BUFFER = 
+BUFFER = 4096
 
-
+usernameToPassword = {}
 
 
 """
@@ -28,8 +28,18 @@ Returns:
     None
 """
 def chatroom (args):
+    sock = args["sock"]
+
     # Task1: login/register the user
-   
+    username = sock.recv(BUFFER).decode()
+    if username in usernameToPassword:
+        sock.send(b"1")
+        password = usernameToPassword[username]
+        sock.send(password.encode())
+    else:
+        sock.send(b"0")
+        password = sock.recv(BUFFER).decode()
+        usernameToPassword[username] = password
 
     # Task2: use a loop to handle the operations (i.e., BM, PM, EX)
     
@@ -40,31 +50,50 @@ def chatroom (args):
 
 if __name__ == '__main__':
     # TODO: Validate input arguments
-   
+    if len(sys.argv) != 2:
+        print("Wrong number of arguments!")
+        sys.exit()
+
+    if sys.argv[1] != "41025":
+        print("Wrong port number!")
+        sys.exit()
+
+    port = int(sys.argv[1])
+    hostname = 'localhost'
+    host = socket.gethostbyname(hostname)
 
     # TODO: create a socket in UDP or TCP
     try:
-
+        serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error as e:
         print('Failed to create socket.')
         sys.exit()
     
     # TODO: Bind the socket to address
     try:
-        serversock.bind(('', PORT))
+        serversock.bind((host, port))
     except socket.error as e:
         print('Failed to bind socket.')
         sys.exit()
 
+    # TODO: start listening
+    try:
+        serversock.listen()
+    except socket.error:
+        print('Failed to listen to clients.')
+        sys.exit()
 
     while True:
-        print(f"Waiting for connections on port {PORT}")
+        print(f"Waiting for connections on port {port}")
 
         # TODO: handle any incoming connection with UDP or TCP
-
+        conn, addr = serversock.accept()
+        args = {"sock": conn, "info": usernameToPassword}
+        print('Connection established.')
 
         # TODO: initiate a thread for the connected user
-        
+        thread = threading.Thread(target=chatroom, args=(args,))
+        thread.start()
        
 
 
